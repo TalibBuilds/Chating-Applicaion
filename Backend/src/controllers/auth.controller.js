1b910cd725a12e23f8d811abd6efedce02a25f8c
Talib <webdevtalib@gmail.com>
2026-07-18 12:35:56 +0530

    Refactor auth controller for production cookie handling
    Updated cookie settings for production environment and improved error messages.

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -41,11 +41,11 @@ const registerUser = async (req, res) => {
             expiresIn: '7d'
         });
 
-        res.cookie('token', token, {
-            httpOnly: true,
-            sameSite: 'strict',
-            secure: false  //only for development
-        });
+       res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+});
 
         const userObj = user.toObject();
         delete userObj.password
@@ -104,12 +104,11 @@ const loginUser = async (req, res) => {
         }
     );
 
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: false,
-        sameSite: "strict"
-    });
-
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+});
     const userObj = isUser.toObject();
     delete userObj.password;
 
