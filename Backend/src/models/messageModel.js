58fa918f62a730133cd8bcad2ed155042baf2bc2
Talib <webdevtalib@gmail.com>
2026-07-18 12:53:57 +0530

    Implement TTL index for message expiration
    Added TTL index to automatically delete messages after 7 days.

--- Backend/src/models/messageModel.js
+++ Backend/src/models/messageModel.js
@@ -25,9 +25,12 @@ const messageSchema = new mongoose.Schema(
             default: false,
         },
     },
-    { timestamps: true } 
+    { timestamps: true }
 );
+
+// TTL Index — createdAt se 7 din baad message automatically delete ho jayega
+messageSchema.index({ createdAt: 1 }, { expireAfterSeconds: 604800 }); // 7 * 24 * 60 * 60 = 604800 seconds
 
 const messageModel = mongoose.model('Message', messageSchema);
 
-module.exports =  messageModel ;
+module.exports = messageModel;
